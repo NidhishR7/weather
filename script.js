7ae784e17590052dcffac8c6a5bb30170b3900b1
NidhishR7 <103636261+NidhishR7@users.noreply.github.com>
2023-02-11 15:03:12 +0530

    Ui Change

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city, opt
 	.then((response) => {       
 console.log(response)
 cloud_pct.innerHTML = response.cloud_pct
-temp.innerHTML = response.temp
+//temp.innerHTML = response.temp
 temp2.innerHTML = response.temp
 feels_like.innerHTML = response.feels_like
-humidity.innerHTML = response.humidity
+//humidity.innerHTML = response.humidity
 humidity2.innerHTML = response.humidity
 min_temp.innerHTML = response.min_temp
 max_temp.innerHTML = response.max_temp
-wind_speed.innerHTML = response.wind_speed
+//wind_speed.innerHTML = response.wind_speed
 wind_speed2.innerHTML = response.wind_speed
-wind_degrees.innerHTML = response.wind_degrees
-sunrise.innerHTML = response.sunrise
-sunset.innerHTML = response.sunset
+// wind_degrees.innerHTML = response.wind_degrees
+// sunrise.innerHTML = response.sunrise
+// sunset.innerHTML = response.sunset
 })
 	.catch(err => console.error(err));
 }
@@ -32,4 +32,4 @@ submit.addEventListener("click", (e)=>{
     e.preventDefault()
     getWeather(city.value)
 })
-getWeather("Delhi")
+getWeather("New Delhi")
